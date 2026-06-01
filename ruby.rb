2447275471e621db43ce507b50145f26c0a3bb05
blob <130174758+BlobCybersecurity@users.noreply.github.com>
2026-06-01 21:49:26 +0200

    Somehow works

--- ruby.rb
+++ ruby.rb
@@ -1,5 +1,5 @@
 def bubble_sort(array)
-  track = 1
+  track = array.length
   timesMissed = 0
   array.each do |v|
     array.each_with_index do |x, i| ## why skips to 2 cuz 4 is less than 78 
@@ -10,27 +10,34 @@ def bubble_sort(array)
           # ## As long as i + 1 is less than a variable we set(equal to the length of the array in the beginning)then were good to go
           ## After the outer loop iteration we make it less to take into account the rightemost value being sorted
           #
-             
-            if x > array[i + 1] 
-              p i
-              p "#{x} > #{array[i + 1]} ??"
-              p "yes"
-              array[i], array[i + 1] = array[i + 1], array[i]
-              p array
-            else
-              timesMissed += 1
-              next
+           
+            if array[i + 1] != nil
+              if  x > array[i + 1] 
+                p "Array i + 1: ", array[i + 1]
+                p x
+                p i
+                p "#{x} > #{array[i + 1]} ??"
+                p "yes"
+                array[i], array[i + 1] = array[i + 1], array[i]
+                p array
+              else
+                 next
+              end
+              
             end
+           
+            
 
-          end
 
-         
 
-      
+          
+
+      break if i + 1 > track
+
      
     end
-    break if track == array.length - 1
-    track += 1
+    track -= 1
+    
     
   end
    return array 
@@ -42,6 +49,7 @@ def bubble_sort(array)
 end
 
  p bubble_sort([4,3,78,2,0,2])
+ p bubble_sort([5, 8, 1, 3, 0, 9]) # 0, 1, 3, 5, 8, 9
 
  ## On the first iteration
  ## Swap places if current item is greater than second
